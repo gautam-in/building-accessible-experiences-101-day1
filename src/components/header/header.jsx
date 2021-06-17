c196bd8e85c5722adc3f7e97061d5386a9799a24
Gautam Chadha <gautam.chadha@publicissapient.com>
2021-06-17 08:58:19 +0530

    Completed header

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,8 +1,17 @@
 import { Logo } from '../logo';
+import { MiniCart } from '../miniCart/miniCart';
+import { Navigation } from '../navigation/navigation';
 import styles from './header.module.css';
 
 export const Header = () => (
   <div className={styles.container}>
     <Logo />
+    <Navigation
+      links={[
+        { id: 1, text: 'Home', url: '/' },
+        { id: 2, text: 'About', url: '/about' },
+      ]}
+    />
+    <MiniCart />
   </div>
-)
+);
